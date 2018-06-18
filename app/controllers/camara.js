a9847d77765e63120fb9d38a7776b7274242f24a
Reynaldo Miguel <rmiguel@cion.com.mx>
2018-06-18 11:19:55 -0500

    Primer comimit demoCamara.

---
+++ app/controllers/camara.js
@@ -0,0 +1,99 @@
+// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+var args = $.args;
+var loginCorrecto = Ti.App.Properties.getBool("loginCorrecto");
+
+function camaraFoto() {
+    Titanium.Media.showCamera({
+
+        saveToPhotoGallery: true,
+        allowEditing: false,
+        autohide: false, //Important!
+
+        success: function(event) {
+
+            var imageViewImagen = Ti.UI.createImageView({
+                width: "200",
+                height: "200",
+                backgroundColor: "blue",
+                top: "10",
+                image: event.media
+            });
+            $.win.add(imageViewImagen);
+
+        },
+
+    });
+
+}
+
+function camaraVideo() {
+    Ti.Media.showCamera({
+        autohide: false,
+        animated: false,
+        allowEditing: false,
+        saveToPhotoGallery: true,
+        success: function(event) {
+            var movie = Titanium.Media.createVideoPlayer({
+                movieControlStyle: Titanium.Media.VIDEO_CONTROL_EMBEDDED,
+                width: "200",
+                height: "200",
+                top: "180",
+                backgroundColor: "#000",
+                autoplay: false
+            });
+            $.win.add(movie);
+            movie.url = event.media.nativePath;
+        },
+        cancel: function(event) {
+            console.log("error");
+        },
+        error: function(error) {
+            alert('error');
+        },
+        mediaTypes: [Titanium.Media.MEDIA_TYPE_VIDEO],
+        videoMaximumDuration: 5000,
+        videoQuality: Titanium.Media.QUALITY_HIGH
+    });
+
+}
+
+function capturaFoto(e) {
+
+    if (!Ti.Media.hasCameraPermissions()) {
+
+        // request permissions to capture media
+        Ti.Media.requestCameraPermissions(function(e) {
+
+            // success! we can capture media!
+            if (e.success) {
+                camaraFoto();
+                // oops! could not obtain required permissions...
+            } else {
+                Ti.API.error('No se pueden obtener permisos de la camara.');
+            }
+        });
+    } else {
+        // yay! we already have permissions!
+        //Ti.Media.showCamera({ ... });
+        camaraFoto();
+    }
+}
+
+function capturaVideo(e) {
+
+    if (!Ti.Media.hasCameraPermissions()) {
+
+        // request permissions to capture media
+        Ti.Media.requestCameraPermissions(function(e) {
+
+            // success! we can capture media!
+            if (e.success) {
+                camaraVideo();
+            } else {
+                Ti.API.error('No se pueden obtener permisos de la camara.');
+            }
+        });
+    } else {
+        camaraVideo();
+    }
+}
---
+++ app/controllers/index.js
@@ -0,0 +1,15 @@
+/*function doClick(e) {
+	alert($.label.text);
+}*/
+function entrar() {
+    if ($.usuarioField.value == "" || $.contraseñaField.value == "") {
+        alert('Usuario y Contraseña son obligatorios.');
+        return;
+    }else {
+    	Ti.App.Properties.setBool("loginCorrecto", true);
+			Alloy.createController('camara').getView().open();
+    }
+
+}
+
+$.index.open();
